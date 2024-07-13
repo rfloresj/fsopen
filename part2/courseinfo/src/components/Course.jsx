da2a3f3fa8c8989a9971ea60b2013042f8be3e97
rflores <104962825+rfloresj@users.noreply.github.com>
2024-07-13 12:32:16 -0600

    course-information-part2-step-9

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,18 +1,24 @@
 const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((acc, val) =>  acc + val.exercises, 0);
   return (
     <>
-      <h1>{course.name}</h1>
-
-      {course.parts.map((part) => (
-        <>
-          <p key={part.id}>
-              {part.name} <span>{part.exercises}</span>
-          </p>
-        </>
-      ))}
-
-      <h3>total of {totalExercises} exercises</h3>
+      <h1>Web development curriculum</h1>
+      {course.map((item) => {
+        const totalExercises = item.parts.reduce((acc, val) => acc + val.exercises, 0)
+        ;
+        return (
+          <>
+            <h3 key={item.id}>{item.name}</h3>
+            {item.parts.map((names) => {
+              return (
+                <p key={names.id}>
+                  {names.name} <span>{names.exercises}</span>
+                </p>
+              );
+            })}
+            <strong>total of {totalExercises} exercises</strong>
+          </>
+        );
+      })}
     </>
   );
 };
